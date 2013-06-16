1e62359179045b5e98f55d28f15a05ca2639fbe3
Christopher Swenson <chris@caswenson.com>
2013-06-15 22:10:29 -0700

    Working on implementing stage 3 of fast aks

--- fast_aks.py
+++ fast_aks.py
@@ -145,12 +145,82 @@ def primitive_root(r):
     else:
       return m
 
+def find_sj(z, pairs, n):
+
+  def add(p, q):
+    if len(q) > len(p):
+      p.extend([0] * (len(q) - len(p)))
+    for i, qi in enumerate(q):
+      p[i] = (p[i] + qi) % n
+
+  def mult(p, q):
+    pq = [0] * (len(p) + len(q))
+    qis = [i for i, qi in enumerate(q) if qi != 0]
+    for i, pi in enumerate(p):
+      if pi == 0: continue
+      for j in qis:
+        qj = q[j]
+        pq[i + j] += (pi * qj) % n
+    return pq
+
+  def multiply(p, q, r):
+    #print "Multiplying %s x %s" % (str([len(x) for x in p]), str([len(x) for x in q]))
+    # lazy schoolbook multiplication
+    pq = [list() for i in xrange(max(r, len(p) + len(q)))]
+    for i, pi in enumerate(p):
+      for j, qj in enumerate(q):
+        add(pq[(i + j) % r], mult(pi, qj))
+    return pq
+
+  def multiply_down(polys, r):
+    print "Mutiplying down %d" % len(polys)
+    if len(polys) == 1:
+      return polys[0]
+    if len(polys) == 2:
+      return multiply(polys[0], polys[1], r)
+    mid = len(polys) / 2
+    return multiply(multiply_down(polys[0:mid], r), multiply_down(polys[mid:], r), r)
+
+  print z
+  print pairs
+
+  gs = []
+
+  for r, q in pairs:
+    polys = []
+    for j in xrange(q):
+      Sj = set()
+      zj = pow(z, j, r)
+      zq = pow(z, q, r)
+      zjlq = zj
+      Sj.add(zjlq)
+      for l in xrange((r - 1) / q):
+        zjlq += zq
+        Sj.add(zjlq)
+      poly = [0] * (max(Sj) + 1)
+      for m in Sj:
+        poly[m] = -1
+      polys.append([[1], poly])
+    g = multiply_down(polys, r)
+    print len(g)
+    gs.append(g)
+
+
+
+
+
+
+
+
+
+
+
 def find_monic(n, pairs):
   # TODO
   # stage 1
   for r, q in pairs:
     z = primitive_root(r)
-    Sj = find_sj(z, pairs)
+    Sj = find_sj(z, pairs, n)
   # stage 2
   # stage 3
 
@@ -164,3 +234,7 @@ def ctz(n):
     z += 1
     n >>= 1
   return z
+
+
+if __name__ == '__main__':
+  print is_prime(31)
